707329d4fd75517957156f520d324fb9d2bffadc
JoniMonetti1 <158229526+JoniMonetti1@users.noreply.github.com>
2024-07-27 19:15:03 -0300

    Se termina de añadir la funcionalidad y se agrega un gif como preloader al video

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const sliderBtn = document.querySelector('.slider-button');
-
-sliderBtn.addEventListener('click', function() {
-    const slider = this.querySelector('.slider');
+document.addEventListener("DOMContentLoaded", () => {
     const video = document.getElementById('background-video');
+    const preloader = document.getElementById('preloader');
+    const switchButton = document.querySelector('.switch-btn');
+    const mainTitle = document.querySelector('.main-title');
+    const overlay = document.querySelector('.overlay');
 
-    if (this.classList.toggle('active')) {
-        slider.style.transform = 'translateX(-200px)';
-        video.play();
-    } else {
-        slider.style.transform = 'translateX(0)';
-        video.pause();
-    }
-})
+    //Inicialmente ocultar todos los elementos relacionados al video
+    switchButton.classList.add('hidden');
+    overlay.classList.add('hidden');
+    mainTitle.classList.add('hidden');
+    video.classList.add('hidden');
+
+    //mostrar el preloader
+    preloader.style.display = 'block';
+
+    //escuchar cuando el video este listo para reproducirse
+    video.addEventListener('canplaythrough', () => {
+        //ocultar el preloader despues de que el video este listo
+        preloader.style.display = 'none';
+
+        //mostrar los elementos relacionados al video
+        switchButton.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+        mainTitle.classList.remove('hidden');
+        video.classList.remove('hidden');
+
+        //inicia la reproducción del video al cargar la página
+        video.play(); 
+    });
+
+    //escucahr cuando se hace click en el boton y manejar su funcionalidad
+    switchButton.addEventListener('click', function() {
+        //alternar la clase slide para reproducir o pausar el video
+        if (this.classList.toggle('slide')) {
+            video.play();
+        } else {
+            video.pause();
+        };
+    });
+});
